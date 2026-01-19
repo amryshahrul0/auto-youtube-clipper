3634e683c4847fdcac8fa0911f424c3b29e12ca4
amryshahrul0 <amryshahrul0@gmail.com>
2026-01-20 03:40:43 +0800

    Add configuration settings for video clips

---
+++ config.py
@@ -0,0 +1,12 @@
+CLIPS_PER_DAY = 8
+
+SOURCE_CHANNELS = [
+    "https://www.youtube.com/@TheDiaryOfACEO",
+    "https://www.youtube.com/@ImpactTheory",
+    "https://www.youtube.com/@MyFirstMillionPod",
+]
+
+MIN_CLIP_SECONDS = 20
+MAX_CLIP_SECONDS = 45
+
+UPLOAD_PRIVACY = "public"
